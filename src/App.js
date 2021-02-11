e80dcb1e4f4872d62544f528f814b19c1a70beed
ChaseReineke <chase.e.reineke@gmail.com>
2021-02-10 23:09:03 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BoxGen from './components/MainComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxGen />
+    </div>
+  );
+}
+
+export default App;
